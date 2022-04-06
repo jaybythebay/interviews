1169838ea394323d21a39318edc0fc6491d71224
Jay Rosenthal <jayrosenthal@US-L379P63DWC.local>
2022-04-06 05:03:47 -0700

    added input:

---
+++ flatten_json_interview/inputs.py
@@ -0,0 +1,50 @@
+bread_production = [
+	{"production_date": "2022-03-01",
+	 "breads":
+		 {"baguettes":
+		            {"sourdough": 15,
+		             "french": 25,
+		             "french demi": 45,
+		             },
+		  "rye":
+			  {"caraway rye": 33.0,
+			   "plain rye": 22
+			   },
+		  "SOURDOUGH": {"cheddar black pepper": 15.0,
+		                "cranberry walnut": 10,
+		                "classic": "    ",
+		                },
+		  },
+	 },
+	{"production_date": "2022-03-02 12:33:00",
+	 "breads":
+		 {"baguettes":
+			  {"sourdough": 22,
+			   "french": 25,
+			   "french demi": 45
+			   },
+		  "rye":
+			  {"caraway rye": 33.0,
+			   "plain rye": 22},
+		  "sourdough": {"cheddar black pepper": 15.0,
+		                "cranberry walnut": 10,
+		                "classic": "    ",
+		                },
+		  },
+	 },
+]
+
+#   },
+#
+#
+#
+#
+# {"cupcakes": {"chocolate": 50,
+#               "red velvet": 66,
+#               "classic vanilla": 22.0,
+#               "lemon": ""},
+#  {"Viennoiserie": {"danish": }
+#   }
+#
+#
+# }

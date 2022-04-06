31df3503f8fa95a13092b387b16f2394acceaa9b
Jay Rosenthal <jayrosenthal@US-L379P63DWC.local>
2022-04-06 05:16:12 -0700

    finished input:

--- flatten_json_interview/inputs.py
+++ flatten_json_interview/inputs.py
@@ -1,11 +1,34 @@
+sweets_production = [
+	{"production_date": "2022-03-01",
+	 "cupcakes": {"chocolate": 50,
+	              "red velvet": 66,
+	              "classic vanilla": 22.0,
+	              "lemon": ""},
+	 "pastry": {"danish": 22,
+	            "croissants": "45",
+	            "morning bun": None,
+	            },
+	 },
+	{"production_date": "2022-03-02",
+	 "cupcakes": {"chocolate": 12,
+	              "red velvet": "  ",
+	              "classic vanilla": 22.0,
+	              "lemon": ""},
+	 "pastry": {"danish": 22,
+	            "croissants": "45",
+	            "morning bun": None,
+	            },
+	 },
+]
+
 bread_production = [
 	{"production_date": "2022-03-01",
 	 "breads":
 		 {"baguettes":
-		            {"sourdough": 15,
-		             "french": 25,
-		             "french demi": 45,
-		             },
+			  {"sourdough": 15,
+			   "french": 25,
+			   "french demi": 45,
+			   },
 		  "rye":
 			  {"caraway rye": 33.0,
 			   "plain rye": 22
@@ -33,18 +56,3 @@ bread_production = [
 		  },
 	 },
 ]
-
-#   },
-#
-#
-#
-#
-# {"cupcakes": {"chocolate": 50,
-#               "red velvet": 66,
-#               "classic vanilla": 22.0,
-#               "lemon": ""},
-#  {"Viennoiserie": {"danish": }
-#   }
-#
-#
-# }
